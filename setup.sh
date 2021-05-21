e787d234211350def064710b3a45fe9472b9dc86
venuraja79 <venuraja79@gmail.com>
2021-05-21 14:22:08 +0530

    first commit

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"venuraja79@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
