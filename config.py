eecaa681e553574db622b48acd0a5ca8a7e9fb20
venuraja79 <venuraja79@gmail.com>
2021-05-22 12:16:11 +0530

    Added scheduler code

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+# scheduler job to bed data from tn site
+DATA_REFRESH_INTERVAL = os.getenv("DATA_REFRESH_INTERVAL", 180)
--- run.py
+++ run.py
@@ -46,6 +46,17 @@ def process_data():
     df.to_csv("tn_covid_beds.csv")
     df_district.to_csv("tn_covid_district_beds.csv")
 
+# Schedule the job
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
+
+from config import DATA_REFRESH_INTERVAL
+
+@sched.scheduled_job('interval', minutes=DATA_REFRESH_INTERVAL)
+def timed_job():
+    print('The refresh data job is starting now!')
+    process_data()
+    print(f"Data Refresh was successful!")
 
 if __name__ == '__main__':
     process_data()
